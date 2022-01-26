1a5e51d70c89f56f9dc21f0d397c48c13d324911
Kors557 <1500459@mail.ru>
2022-01-26 22:42:33 +0300

    пока что работает

---
+++ yatube/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Записи группы {slug}')
+
+# Create your views here.
